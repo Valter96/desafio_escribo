ada42640c79500474c31f0e6e78bd214fc3c1b5f
Valter Filho <v.a.l_t.e.r@hotmail.com>
2023-11-26 18:06:45 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function somaNumDivisiveis(num){
+
+    // Tratamento caso o usuário insira um valor negativo ou igual a 0
+    if(num <= 0)
+        alert('Informe um número positivo maior que 0')
+
+    let count = 0 // Variavel de controle do loop
+    let numDivisor = 0 // Recebe os numeros divisores por 3 e 5
+    let somaNum = 0 // Soma os numeros recebidos
+
+    // Enquanto count for menor que o número informado pelo usuário executa este código
+    while(count < num){
+        
+        // Seleciona os numeros divisiveis por 3 e 5
+        if(count % 3 === 0 || count % 5 === 0){
+            numDivisor = count
+            somaNum += numDivisor // armazena a soma dos números
+        }
+        count++
+    }
+
+    return somaNum
+}
+
+let insiraValor = prompt("Insira um número positivo: ")
+
+alert('Total da soma: ' + somaNumDivisiveis(insiraValor))
+
+
+
+
+
+
+
+
+
